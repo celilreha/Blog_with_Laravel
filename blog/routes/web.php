a6926070d9fada19cb13315a3cc13b6954a5881d
celilreha <celilrehaesgice@gmail.com>
2021-03-10 00:21:51 +0300

    adding Admin Panel and Admin Login

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,16 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Front\Homepage;
+use App\Http\Controllers\Back\Dashboard;
+use App\Http\Controllers\Back\Auth;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Backend Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+*/
+Route::get("admin/panel",[Dashboard::class,"index"])->name("admin.dashboard");
+Route::get("admin/giris",[Auth::class,"login"])->name("admin.login");
+
+
+/*
+|--------------------------------------------------------------------------
+| Front Routes
+|--------------------------------------------------------------------------
 */
 
 Route::get('/',[Homepage::class,"index"])->name("homepage");
